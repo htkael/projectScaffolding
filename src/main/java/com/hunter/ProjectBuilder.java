7201874cf5d212312e5a71d08f953518b454d4b3
Hunter Kael <htkael@gmail.com>
2026-02-15 23:59:05 -0600

    chore: continuing class

--- src/main/java/com/hunter/ProjectBuilder.java
+++ src/main/java/com/hunter/ProjectBuilder.java
@@ -4,16 +4,51 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class ProjectBuilder {
+  private String userName;
+  private String projectName;
 
-  public static String getCurrentDir() {
+  public static void main(String[] args) {
+    // int exitCode = 0;
+    // if (args.length < 1) {
+    // System.out.println("✗ Invalid commit message");
+    // System.out.println("Error: No arguments provided");
+    // exitCode = -1;
+    // System.exit(exitCode);
+    // }
+    // String input = args[0];
+    int exitCode = 0;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Provide directory name:");
+
+    String dirName = scanner.next();
+    this.dirName = dirName;
+    try {
+      ProjectBuilder.createNestedDirectories(dirName);
+    } catch (IOException e) {
+      exitCode = -1;
+      System.err.println(e.getMessage());
+      System.exit(exitCode);
+    }
+
+    scanner.close();
+    System.exit(exitCode);
+  }
+
+  private static String getCurrentDir() {
     return System.getProperty("user.dir");
   }
 
-  public static void createDirectory(String inDir, String dirName) throws IOException {
-    System.out.println("In dir: " + inDir);
-    Path path = Paths.get(inDir + "/" + dirName);
+  private static String getUserName() {
+    return System.getProperty("user.name");
+  }
+
+  private static void createNestedDirectories(String dirs) throws IOException {
+    Path path = Paths.get(getCurrentDir() + "/" + dirs);
 
     try {
       Files.createDirectories(path);
@@ -22,6 +57,15 @@ public class ProjectBuilder {
       System.err.println("Failed to create directory: " + e.getMessage());
       e.printStackTrace();
       throw new IOException("Failed to create directory: " + e.getMessage());
+    }
+  }
+
+  private static void writeFile(String pathName, String content) throws IOException {
+    try {
+    } catch (IOException e) {
+      System.err.println("Failed to write file: " + e.getMessage());
+      e.printStackTrace();
+      throw new IOException("Failed to write file: " + e.getMessage());
     }
   }
 }
