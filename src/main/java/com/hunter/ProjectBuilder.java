5483b94b89f53ebf212a40ebd2890072894221aa
Hunter Kael <htkael@gmail.com>
2026-02-17 00:20:34 -0600

    feat: added file writing for pom and Main files

--- src/main/java/com/hunter/ProjectBuilder.java
+++ src/main/java/com/hunter/ProjectBuilder.java
@@ -4,44 +4,31 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class ProjectBuilder {
 
   public static void main(String[] args) {
-    // int exitCode = 0;
-    // if (args.length < 1) {
-    // System.out.println("✗ Invalid commit message");
-    // System.out.println("Error: No arguments provided");
-    // exitCode = -1;
-    // System.exit(exitCode);
-    // }
-    // String input = args[0];
     int exitCode = 0;
+    if (args.length < 1) {
+      System.out.println(
+          "Invalid arguments. Please provide a single string representing the name of the project you would like to create.");
+      exitCode = -1;
+      System.exit(exitCode);
+    }
+    String projectName = args[0];
 
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.println("Provide directory name:");
-
-    String dirName = scanner.next();
     try {
-      ProjectBuilder.createNestedDirectories(dirName);
+      ProjectBuilder.createNestedDirectories(projectName + "/src/main/java/com/hunter");
+      ProjectBuilder.createNestedDirectories(projectName + "/src/test/java/com/hunter");
+      ProjectBuilder.createPomFile(projectName);
+      ProjectBuilder.createMainFile(projectName);
     } catch (IOException e) {
       exitCode = -1;
       System.err.println(e.getMessage());
       System.exit(exitCode);
     }
 
-    scanner.close();
     System.exit(exitCode);
-  }
-
-  private static String getCurrentDir() {
-    return System.getProperty("user.dir");
-  }
-
-  private static String getUserName() {
-    return System.getProperty("user.name");
   }
 
   private static void createNestedDirectories(String dirs) throws IOException {
@@ -49,7 +36,6 @@ public class ProjectBuilder {
 
     try {
       Files.createDirectories(path);
-      System.out.println("Directory created: " + path.toAbsolutePath());
     } catch (IOException e) {
       System.err.println("Failed to create directory: " + e.getMessage());
       e.printStackTrace();
@@ -57,12 +43,34 @@ public class ProjectBuilder {
     }
   }
 
-  private static void createFile(String pathName, String content) throws IOException {
+  private static void createPomFile(String projectName) throws IOException {
     try {
+      Path whereTo = Paths.get(getCurrentDir() + "/" + projectName + "/pom.xml");
+      Path templatePath = Paths.get("pom.template.xml");
+      String template = Files.readString(templatePath);
+      String toWrite = template.replace("{{PROJECT_NAME}}", projectName);
+      Files.writeString(whereTo, toWrite);
     } catch (IOException e) {
-      System.err.println("Failed to write file: " + e.getMessage());
+      System.err.println("Failed to write pom file: " + e.getMessage());
       e.printStackTrace();
-      throw new IOException("Failed to write file: " + e.getMessage());
+      throw new IOException("Failed to write pom file: " + e.getMessage());
     }
+  }
+
+  private static void createMainFile(String projectName) throws IOException {
+    try {
+      Path whereTo = Paths.get(getCurrentDir() + "/" + projectName + "/src/main/java/com/hunter/Main.java");
+      Path templatePath = Paths.get("Main.template.java");
+      String toWrite = Files.readString(templatePath);
+      Files.writeString(whereTo, toWrite);
+    } catch (IOException e) {
+      System.err.println("Failed to write Main.java file: " + e.getMessage());
+      e.printStackTrace();
+      throw new IOException("Failed to write Main.java file: " + e.getMessage());
+    }
+  }
+
+  private static String getCurrentDir() {
+    return System.getProperty("user.dir");
   }
 }
